d236fde2f154e14702dcb710a04c082b44cc9193
hendurhance <afriviral@gmail.com>
2021-10-11 22:42:53 +0100

    resource controller --added

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+/* ======== PRODUCTS ======== */
+
+Route::apiResource('products', ProductController::class);
+
+/* ======== REVIEWS ======== */
+
+Route::prefix('products')->group(function () {
+    Route::apiResource('{product}/reviews', ReviewController::class);
 });
